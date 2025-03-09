2309ae1e2cefa2594f46f24e1e1715a7b3183c0e
devpatel2103 <devpatel2103@gmail.com>
2025-03-09 00:44:52 -0500

    Changed some stuff

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginHome() {
+    return (
+        <div>
+            <h1>Login Here</h1>
+            <a href="../" className="text-blue-500 hover:text-blue-700 underline">Back to Home</a>
+        </div>
+    );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,8 @@
+export default function RegisterHome() {
+    return (
+        <div>
+            <h1>Register Here</h1>
+            <a href="../" className="text-blue-500 hover:text-blue-700 underline">Back to Home</a>
+        </div>
+    );
+}
